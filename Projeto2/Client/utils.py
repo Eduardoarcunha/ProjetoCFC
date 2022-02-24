ffce75c9209e0f11e12642ba9ba63ada339f863e
Eduardoarcunha12 <eduardoarc@al.insper.edu.br>
2022-02-24 15:13:09 -0300

    teste

---
+++ Projeto2/Client/utils.py
@@ -0,0 +1,22 @@
+import random
+
+def randomCommands():
+    Comands = []
+    ComandsDic = {
+            "COMAND1": "00 FF 00 FF",
+            "COMAND2": "00 FF FF 00",
+            "COMAND3": "FF",
+            "COMAND4": "00",
+            "COMAND5": "FF 00",
+            "COMAND6": "00 FF",
+    }
+
+    nComands = random.randint(10,30)
+
+    for n in range(nComands):
+        i = random.randint(1,6)
+        Comands.append(ComandsDic['COMAND' + str(i)])
+
+    return Comands
+
+#print(randomCommands())
